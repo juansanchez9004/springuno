4242e084ac4f79052147b9ad2028057ddbd0df69
juansanchez9004 <juantabasan@gmail.com>
2022-04-12 20:55:40 -0500

    Fuentes iniciales del proyecto uno de spring boot

---
+++ src/main/java/com/application/springuno/SpringunoApplication.java
@@ -0,0 +1,13 @@
+package com.application.springuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/springuno/SpringunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.springuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
